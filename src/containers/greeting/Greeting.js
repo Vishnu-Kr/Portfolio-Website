7366859d54546ea960e677af45bb4bccb3aa22fc
Vishnu-Kr <vishnu.kumardgp2002@gmail.com>
2023-02-27 06:38:45 +0530

    Changed the template for adding profile picture

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {createRef,useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
@@ -12,6 +12,7 @@ import StyleContext from "../../contexts/StyleContext";
 
 export default function Greeting() {
   const {isDark} = useContext(StyleContext);
+  const imgRef = createRef();
   if (!greeting.displayGreeting) {
     return null;
   }
@@ -20,6 +21,19 @@ export default function Greeting() {
       <div className="greet-main" id="greeting">
         <div className="greeting-main">
           <div className="greeting-text-div">
+            <div>
+            {logo && (
+            <div className="greeting-text-div-dp">
+              <img
+                crossOrigin={"anonymous"}
+                ref={imgRef}
+                className="greeting-text-div-dp"
+                src={logo}
+                alt={username}
+              />
+            </div>
+          )}
+            </div>
             <div>
               <h1
                 className={isDark ? "dark-mode greeting-text" : "greeting-text"}
